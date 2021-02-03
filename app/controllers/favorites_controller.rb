c61dfa5f2595b4340b5031c4f13fafec8db1c247
masutaniR <popohu87@gmail.com>
2021-02-03 06:38:53 +0000

    [Start] 課題3 コメント機能

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -8,7 +8,7 @@ class FavoritesController < ApplicationController
 
   def destroy
     book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(params[book_id: book.id])
+    favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
     redirect_back(fallback_location: books_path)
   end
