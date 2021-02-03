70a5803da6184c87f235d103cf0498e122d19f92
masutaniR <popohu87@gmail.com>
2021-02-03 07:40:17 +0000

    [Finish] 課題3

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,4 +1,6 @@
 class BookCommentsController < ApplicationController
+  before_action :correct_ensure_user, only: [:destroy]
+
   def create
     @book = Book.find(params[:book_id])
     @comment = current_user.book_comments.new(book_comment_params)
@@ -19,5 +21,12 @@ class BookCommentsController < ApplicationController
   private
     def book_comment_params
       params.require(:book_comment).permit(:comment)
+    end
+
+    def correct_ensure_user
+      @comment = BookComment.find(params[:id])
+      unless @comment.user == current_user
+        redirect_to current_user
+      end
     end
 end
