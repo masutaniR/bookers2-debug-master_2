bb6a7a4df0465e03f56686f121e0c337527fc97a
masutaniR <popohu87@gmail.com>
2021-02-04 07:06:30 +0000

    [Finish] 課題4

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update] do
     member do
-      get :following, :followers
+      get :followings, :followers
     end
     resource :relationships, only: [:create, :destroy]
   end
