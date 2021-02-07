eb98e2e00e38ed8c55f7af46ed65fcbf1564a281
masutaniR <popohu87@gmail.com>
2021-02-07 03:11:15 +0000

    [Start] 課題6 コメント非同期

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,17 +5,13 @@ class BookCommentsController < ApplicationController
     @book = Book.find(params[:book_id])
     @comment = current_user.book_comments.new(book_comment_params)
     @comment.book_id = @book.id
-    if @comment.save
-      redirect_to book_path(@book)
-    else
-      @book_new = Book.new
-      render 'books/show'
+    unless @comment.save
+      render 'error'
     end
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
   end
 
   private
