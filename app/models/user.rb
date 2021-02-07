8893aa73c589afb09244c42078fabf6ab6304ba9
masutaniR <popohu87@gmail.com>
2021-02-07 01:26:03 +0000

    [Add] 課題5 検索機能,検索結果ビュー

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,17 @@ class User < ApplicationRecord
   def following?(user)
     following.include?(user)
   end
+
+  # 検索用メソッド
+  def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE?', "#{content}%")
+    elsif method == 'backward'
+      User.where('name LIKE?', "%#{content}")
+    else
+      User.where('name LIKE?', "%#{content}%")
+    end
+  end
 end
