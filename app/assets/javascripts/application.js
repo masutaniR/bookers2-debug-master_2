195c2f56b200fad07e33a6a9a2c70c3cf10d11ad
masutaniR <popohu87@gmail.com>
2021-02-09 01:16:27 +0000

    [Finish] 課題7

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,14 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+$(function () {
+  return $('#user_postal_code').jpostal({
+      postcode: ['#user_postal_code'],
+      address: {
+        "#user_prefecture_code": "%3",
+        "#user_city": "%4%5",
+        "#user_street": "%6%7"
+      }
+  });
+});
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  include JpPrefecture
+  jp_prefecture :prefecture_code
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -38,5 +40,14 @@ class User < ApplicationRecord
     else
       User.where('name LIKE?', "%#{content}%")
     end
+  end
+  
+  # 都道府県コードを都道府県名に変換
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+  
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
   end
 end
