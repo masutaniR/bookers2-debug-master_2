7c59a275c7e9199677f646fdddaee7ed8a2a9283
masutaniR <popohu87@gmail.com>
2021-02-09 08:31:09 +0000

    [Add] 課題9 モデル間のアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@ class User < ApplicationRecord
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :user_rooms
+  has_many :chats
 
   def follow(user)
     relationships.create(followed_id: user.id)
