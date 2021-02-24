6147943c342e5c024fb847692046be1be4662986
masutaniR <popohu87@gmail.com>
2021-02-24 03:11:34 +0000

    [Update] ユーザー登録処理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top, :about]
-	before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
   def after_sign_in_path_for(resource)
@@ -9,9 +8,5 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     root_path
-  end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :postal_code, :prefecture_code, :city, :street])
   end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -70,6 +70,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   protected
     def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :postal_code, :prefecture_code, :city, :street])
       devise_parameter_sanitizer.permit(:account_update, keys: [:email])
     end
 end
