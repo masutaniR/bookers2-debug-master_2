4505b65ab0b2dc530a0d7de4a111f672df49fbc7
masutaniR <popohu87@gmail.com>
2021-02-25 01:54:35 +0000

    [Add] Notification model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_09_082318) do
+ActiveRecord::Schema.define(version: 2021_02_25_014931) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -41,6 +41,21 @@ ActiveRecord::Schema.define(version: 2021_02_09_082318) do
     t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.integer "visitor_id", null: false
+    t.integer "visited_id", null: false
+    t.integer "book_id"
+    t.integer "book_comment_id"
+    t.string "action", default: "", null: false
+    t.boolean "checked", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_comment_id"], name: "index_notifications_on_book_comment_id"
+    t.index ["book_id"], name: "index_notifications_on_book_id"
+    t.index ["visited_id"], name: "index_notifications_on_visited_id"
+    t.index ["visitor_id"], name: "index_notifications_on_visitor_id"
   end
 
   create_table "relationships", force: :cascade do |t|
