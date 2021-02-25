3b7a114c78e48369c8ba8ceef2d604dabe9e71a6
masutaniR <popohu87@gmail.com>
2021-02-25 02:19:58 +0000

    [Add] メソッド定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,4 +54,16 @@ class User < ApplicationRecord
   def prefecture_name=(prefecture_name)
     self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
   end
+
+  # フォロー通知
+  def create_notification_follow(current_user)
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and action = ?", current_user.id, id, 'follow'])
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        visited_id: id,
+        action: 'follow'
+        )
+      notification.save if notification.valid?
+    end
+  end
 end
