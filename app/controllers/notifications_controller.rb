8d1f9223210ae4de129c3d7407fd053b6c74125e
masutaniR <popohu87@gmail.com>
2021-02-27 23:46:29 +0000

    [Update] 通知 時間表示

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,7 @@
 class NotificationsController < ApplicationController
   def index
     @notifications = current_user.passive_notifications.all
+    # sleep(5)
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
